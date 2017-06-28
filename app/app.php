b756a2b35eb38486d2d8824e34a6694c37b436fd
Brittany Kerr and Michelle Poterek <brittany@>
2017-06-28 11:04:50 -0700

    add return function

--- app/app.php
+++ app/app.php
@@ -1,17 +1,54 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Task.php";
-
-    session_start();
-    if (empty($_SESSION['list_of_tasks'])) {
-        $_SESSION['list_of_tasks'] = array();
-    }
+    require_once __DIR__."/../src/places.php";
+    //
+    // session_start();
+    // if (empty($_SESSION['city'])) {
+    //     $_SESSION['city'] = array();
+    // }
 
     $app = new Silex\Application();
 
-    $app->get("/hello", function() {
-      return "Hello friend!";
+    $app->get("/home", function() {
+      return "<!DOCTYPE html>
+      <html>
+          <head>
+              <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'>
+              <meta charset='utf-8'>
+              <title>Places!</title>
+          </head>
+          <body>
+              <div class='container'>
+                  <form action='/places'>
+                      <div class='form-group'>
+                          <label for='city'>What city have you been to?</label>
+                          <input name='city' class='form-control' type='text'>
+                      </div>
+                      <div class='form-group'>
+                          <label for='favorite_food'>What was the best food you tasted?</label>
+                          <input name='favorite_food' class='form-control' type='text'>
+                      </div>
+                      <div class='form-group'>
+                          <label for='destination'>What was your favorite destination?</label>
+                          <input name='destination' class='form-control' type='text'>
+                      </div>
+                      <button type='submit' class='btn-success'>Submit!</button>
+                  </form>
+              </div>
+          </body>
+      </html>";
+
+    });
+
+    $app->get("/places", function () {
+        $city = new Place($_GET['city'], $_GET['favorite_food'], $_GET['destination']);
+
+            return "<h1>Wow look at where you went</h1>
+            <p> ". $city->getCity() . "</p>
+            <p> ". $city->getFood() .  "</p>
+            <p> ". $city->getDestination() . "</p>";
+
     });
 
     return $app;
--- src/places.php
+++ src/places.php
@@ -1,17 +1,53 @@
+<?php
+    class Place
+    {
+        private $city;
+        private $favorite_food;
+        private $destination;
+
+        function __construct ($city, $favorite_food, $destination)
+        {
+            $this->city = $city;
+            $this->food = $favorite_food;
+            $this->destination = $destination;
+        }
 
 
+        function getCity()
+        {
+            return $this->city;
+        }
+        function getFood()
+        {
+            return $this->food;
+        }
+        function getDestination()
+        {
+            return $this->destination;
+        }
 
+        function setCity($new_city)
+        {
+            $this->city = $new_city;
+        }
 
+        function setFood($new_food)
+        {
+            $this->food = $new_food;
+        }
 
+        function setDestination($new_destination)
+        {
+            $this->destination = $new_destination;
+        }
 
-
-
-
-function save()
-{
-    array_push($_SESSION['list_of_tasks'], $this);
-}
- static function getAll()
- {
-    return $_SESSION['list_of_tasks'];
- }
+        // function save()
+        // {
+        //     array_push($_SESSION['city'], $this);
+        // }
+        //  static function getAll()
+        //  {
+        //     return $_SESSION['city'];
+        //  }
+    }
+?>
